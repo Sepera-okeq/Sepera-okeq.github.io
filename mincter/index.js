800773596657cc7604b820cdd96332d1be8122fb
Sepera-okeq <54331959+Sepera-okeq@users.noreply.github.com>
2020-01-11 23:21:53 +0400

    update-minister

--- mincter/index.js
+++ mincter/index.js
@@ -1,6 +1,7 @@
    
    $(document).ready(function()
    {
+      $("#Carousel1").bootstrapcarousel({interval:3000, pause: false});
       $(document).on('click','.ThemeableMenu1-navbar-collapse.in',function(e)
       {
          if ($(e.target).is('a') && ($(e.target).attr('class') != 'dropdown-toggle')) 
@@ -8,69 +9,6 @@
             $(this).collapse('hide');
          }
       });
-      $("#Accordion2 .panel").on('show.bs.collapse', function()
-      {
-         $(this).addClass('active');
-      });
-      $("#Accordion2 .panel").on('hide.bs.collapse', function()
-      {
-         $(this).removeClass('active');
-      });
-      $("#Accordion3 .panel").on('show.bs.collapse', function()
-      {
-         $(this).addClass('active');
-      });
-      $("#Accordion3 .panel").on('hide.bs.collapse', function()
-      {
-         $(this).removeClass('active');
-      });
-      $("#Accordion4 .panel").on('show.bs.collapse', function()
-      {
-         $(this).addClass('active');
-      });
-      $("#Accordion4 .panel").on('hide.bs.collapse', function()
-      {
-         $(this).removeClass('active');
-      });
-      $("#Progressbar1").progressbar(
-      {
-         value: 100,
-         change: function() 
-         {
-            $("#Progressbar1-label").text($(this).progressbar('value') + '%');
-         }
-      });
-      $("#Progressbar2").progressbar(
-      {
-         value: 80,
-         change: function() 
-         {
-            $("#Progressbar2-label").text($(this).progressbar('value') + '%');
-         }
-      });
-      $("#Progressbar3").progressbar(
-      {
-         value: 60,
-         change: function() 
-         {
-            $("#Progressbar3-label").text($(this).progressbar('value') + '%');
-         }
-      });
-      $("#Progressbar4").progressbar(
-      {
-         value: 1,
-         change: function() 
-         {
-            $("#Progressbar4-label").text($(this).progressbar('value') + '%');
-         }
-      });
-      $("#ListView1").listview(
-      {
-         inset: false
-      });
-      $("#Carousel1").bootstrapcarousel({interval:3000, pause: false});
-      $("#ListView2").listview(
-      {
-         inset: false
-      });
+      $("a[data-rel='gallery']").attr('rel', 'gallery');
+      $("#gallery").magnificPopup({delegate:'a', type:'image', gallery: {enabled: true, navigateByImgClick: true}});
    });
