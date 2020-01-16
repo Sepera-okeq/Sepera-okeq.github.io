2fcf9c42e73e99cd92f2e7e1e17eff40a13dcb74
Sepera-okeq <54331959+Sepera-okeq@users.noreply.github.com>
2020-01-17 00:58:24 +0400

    update-prev-pral

---
+++ vibots_parlament/index.js
@@ -0,0 +1,52 @@
+am4core.useTheme(am4themes_animated);
+
+var chart = am4core.createFromConfig({
+  "data": [{
+    party: "НПР",
+    litres: 43
+  },
+  {
+    party: "Брак",
+    litres: 9
+  },
+  {
+    party: "АПЭ",
+    litres: 15
+  },
+  {
+    party: "ОКП",
+    litres: 16
+  },
+  {
+    party: "ОУН",
+    litres: 16
+  }
+  
+  {
+    party: "ЛДПР",
+    litres: 15
+  },
+  {
+    party: "ВАМЭ",
+    litres: 120
+  }
+  /*,
+  {
+    party: "Belgium",
+    litres: 60
+  },
+  {
+    party: "The Netherlands",
+    litres: 50
+  
+  }*/ ],
+  "legend": {},
+  "innerRadius": "40%",
+  "series": [{
+    "type": "PieSeries3D",
+    "dataFields": {
+      "value": "litres",
+      "category": "party"
+    }
+  }]
+}, "chartdiv", "PieChart3D");
