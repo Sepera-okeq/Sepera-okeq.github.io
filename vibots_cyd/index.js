ddccb0b7b46f79e0e8e2765d8571f4ade66b5db9
Sepera-okeq <54331959+Sepera-okeq@users.noreply.github.com>
2020-01-17 12:56:48 +0400

    update cud
    Добавлен раздел сайта СУД_выборы

---
+++ vibots_cyd/index.js
@@ -0,0 +1,52 @@
+am4core.useTheme(am4themes_animated);
+
+var chart = am4core.createFromConfig({
+  "data": [{
+    cydia: "Неззер",
+    litres: 43
+  },
+  {
+    cydia: "Брак",
+    litres: 9
+  },
+  {
+    cydia: "Молдован",
+    litres: 15
+  },
+  {
+    cydia: "Зеля",
+    litres: 16
+  },
+  {
+    cydia: "ExE",
+    litres: 16
+  }
+  
+  {
+    cydia: "Хаус",
+    litres: 15
+  },
+  {
+    cydia: "Пидорович",
+    litres: 120
+  }
+  /*,
+  {
+    cydia: "Belgium",
+    litres: 60
+  },
+  {
+    cydia: "The Netherlands",
+    litres: 50
+  
+  }*/ ],
+  "legend": {},
+  "innerRadius": "40%",
+  "series": [{
+    "type": "PieSeries3D",
+    "dataFields": {
+      "value": "litres",
+      "category": "cydia"
+    }
+  }]
+}, "chartdiv", "PieChart3D");
