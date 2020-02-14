d0856180da0b1eb785853bd0ab9edbdc6b415d42
Sepera_okeq <sepera173@gmail.com>
2020-02-14 17:30:27 +0400

    fix

--- etc/game-parody-dino-chrome/lib/config.js
+++ etc/game-parody-dino-chrome/lib/config.js
@@ -25,12 +25,12 @@ export default {
     cactusDoubleB: { h: 92, w: 80, x: 184, y: 31 },
     cactusTriple: { h: 66, w: 82, x: 266, y: 31 },
     cloud: { h: 28, w: 92, x: 794, y: 31 },
-    dino: { h: 86, w: 80, x: 350, y: 31 },
-    dinoDuckLeftLeg: { h: 52, w: 110, x: 596, y: 31 },
-    dinoDuckRightLeg: { h: 52, w: 110, x: 596, y: 85 },
-    dinoLeftLeg: { h: 86, w: 80, x: 432, y: 31 },
-    dinoRightLeg: { h: 86, w: 80, x: 514, y: 31 },
-    ground: { h: 28, w: 2400, x: 0, y: 2 },
+    dino: { h: 86, w: 80, x: 350, y: 26 },
+    dinoDuckLeftLeg: { h: 52, w: 110, x: 596, y: 26 },
+    dinoDuckRightLeg: { h: 52, w: 110, x: 596, y: 83 },
+    dinoLeftLeg: { h: 86, w: 80, x: 432, y: 26 },
+    dinoRightLeg: { h: 86, w: 80, x: 514, y: 26 },
+    ground: { h: 24, w: 2400, x: 0, y: 2 },
     replayIcon: { h: 60, w: 68, x: 0, y: 31 }
   }
 }
